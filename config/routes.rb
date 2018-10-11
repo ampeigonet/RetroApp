f58b9c1d64d29f66e9a6fd7bd7aaf87004e421ee
Ana Machado <ana@wyeworks.com>
2018-10-11 11:18:14 -0300

    Provide only /retros/new route for retros

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :retros
+  resources :retros, :only => [:new] # this will change when more features get implemented for retros
   get '/' => 'greetings#hello'
 end
